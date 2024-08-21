08083599c4f3c5a0b4505898a9a0e59f79496e99
AaronHsieh <piafyoyo06@gmail.com>
2024-08-21 23:24:28 +0800

    修改 Login 頁面元件

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,9 +1,30 @@
 <script setup>
-
+import LogoAndDecoImage from "@/components/LogoAndDecoImage.vue";
+import LoginForm from "@/components/LoginForm.vue";
 </script>
 
 <template>
-  <main>
-    123
-  </main>
+  <section class="container">
+    <LogoAndDecoImage />
+    <LoginForm />
+  </section>
 </template>
+
+<style scoped>
+.container {
+  background-color: #ffd370;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 106px;
+  height: 100vh;
+  padding: 0 30px;
+}
+
+@media (max-width: 576px) {
+  .container {
+    flex-direction: column;
+    gap: 16px;
+  }
+}
+</style>
