18b1b6fac3e6548927eccb32a6e317d4e24cd3ca
AaronHsieh <piafyoyo06@gmail.com>
2024-08-21 23:33:04 +0800

    新增註冊頁面 router 元件

---
+++ src/views/RegisterView.vue
@@ -0,0 +1,30 @@
+<script setup>
+import LogoAndDecoImage from "@/components/LogoAndDecoImage.vue";
+import RegisterForm from "@/components/RegisterForm.vue";
+</script>
+
+<template>
+  <section class="container">
+    <LogoAndDecoImage />
+    <RegisterForm />
+  </section>
+</template>
+
+<style scoped>
+.container {
+  background-color: #ffd370;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 106px;
+  height: 100vh;
+  padding: 0 30px;
+}
+
+@media (max-width: 576px) {
+  .container {
+    flex-direction: column;
+    gap: 26px;
+  }
+}
+</style>
