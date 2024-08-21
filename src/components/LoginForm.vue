5541f7cf0c35b2ed290e792d4ec712aa342be7b1
AaronHsieh <piafyoyo06@gmail.com>
2024-08-21 23:56:55 +0800

    新增當欄位沒有被正確填寫時出現錯誤訊息的功能

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,27 +1,56 @@
 <script setup>
-import { RouterLink } from "vue-router";
+import { ref } from "vue";
+
+const email = ref("");
+const password = ref("");
+const emailError = ref(false);
+const passwordError = ref(false);
+
+const handleInput = (field) => {
+  if (field === "email") {
+    emailError.value = !email.value;
+  } else if (field === "password") {
+    passwordError.value = !password.value;
+  }
+};
+
+const handleSubmit = () => {
+  emailError.value = !email.value;
+  passwordError.value = !password.value;
+  if (!emailError.value && !passwordError.value) {
+    // 處理登入邏輯
+  }
+};
 </script>
 
 <template>
-  <form class="formControls">
+  <form class="formControls" @submit.prevent="handleSubmit">
     <h2 class="formControls_txt">最實用的線上代辦事項服務</h2>
     <label class="formControls_label" for="email">Email</label>
     <input
       class="formControls_input"
       type="email"
       name="email"
+      v-model="email"
+      @input="handleInput('email')"
       placeholder="請輸入 email"
       required />
-    <span>此欄位不可留空</span>
+    <span v-if="emailError">此欄位不可留空</span>
     <label class="formControls_label" for="password">密碼</label>
     <input
       class="formControls_input"
       type="password"
       name="password"
-      id="password"
+      v-model="password"
+      @input="handleInput('password')"
       placeholder="請輸入密碼"
       required />
-    <input class="formControls_btnSubmit" type="button" value="登入" />
+    <span v-if="passwordError">此欄位不可留空</span>
+    <input
+      class="formControls_btnSubmit"
+      type="button"
+      value="登入"
+      @click="handleSubmit" />
     <RouterLink to="/register" class="formControls_btnLink"
       >註冊帳號</RouterLink
     >
--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -1,41 +1,102 @@
 <script setup>
-import { RouterLink } from "vue-router";
+import { ref } from "vue";
+
+const email = ref("");
+const nickname = ref("");
+const password = ref("");
+const checkPassword = ref("");
+
+const emailError = ref(false);
+const nicknameError = ref(false);
+const passwordError = ref(false);
+const checkPasswordError = ref(false);
+
+const handleInput = (field) => {
+  switch (field) {
+    case "email":
+      emailError.value = !email.value;
+      break;
+    case "nickname":
+      nicknameError.value = !nickname.value;
+      break;
+    case "password":
+      passwordError.value = !password.value;
+      break;
+    case "checkPassword":
+      checkPasswordError.value = !checkPassword.value;
+      break;
+  }
+};
+
+const handleSubmit = () => {
+  emailError.value = !email.value;
+  nicknameError.value = !nickname.value;
+  passwordError.value = !password.value;
+  checkPasswordError.value = !checkPassword.value;
+
+  if (
+    !emailError.value &&
+    !nicknameError.value &&
+    !passwordError.value &&
+    !checkPasswordError.value
+  ) {
+    // 處理註冊邏輯
+  }
+};
 </script>
 
 <template>
-  <form class="formControls">
+  <form class="formControls" @submit.prevent="handleSubmit">
     <h2 class="formControls_txt">註冊帳號</h2>
     <label class="formControls_label" for="email">Email</label>
     <input
       class="formControls_input"
       type="email"
       name="email"
+      v-model="email"
+      @input="handleInput('email')"
       placeholder="請輸入 email"
       required />
+    <span v-if="emailError">此欄位不可留空</span>
+
     <label class="formControls_label" for="nickname">您的暱稱</label>
     <input
       class="formControls_input"
       type="text"
       name="nickname"
+      v-model="nickname"
+      @input="handleInput('nickname')"
       placeholder="請輸入您的暱稱"
       required />
+    <span v-if="nicknameError">此欄位不可留空</span>
+
     <label class="formControls_label" for="password">密碼</label>
     <input
       class="formControls_input"
       type="password"
       name="password"
-      id="password"
+      v-model="password"
+      @input="handleInput('password')"
       placeholder="請輸入密碼"
       required />
+    <span v-if="passwordError">此欄位不可留空</span>
+
     <label class="formControls_label" for="check-password">再次輸入密碼</label>
     <input
       class="formControls_input"
       type="password"
       name="check-password"
-      id="check-password"
+      v-model="checkPassword"
+      @input="handleInput('checkPassword')"
       placeholder="請再次輸入密碼"
       required />
-    <input class="formControls_btnSubmit" type="button" value="註冊帳號" />
+    <span v-if="checkPasswordError">此欄位不可留空</span>
+
+    <input
+      class="formControls_btnSubmit"
+      type="button"
+      value="註冊帳號"
+      @click="handleSubmit" />
     <RouterLink to="/" class="formControls_btnLink">登入</RouterLink>
   </form>
 </template>
