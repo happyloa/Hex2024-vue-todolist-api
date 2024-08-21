5773bbddd3a1ae325f9b9686d24e8bafc3923db5
AaronHsieh <piafyoyo06@gmail.com>
2024-08-21 23:41:43 +0800

    引入並使用註冊頁面 View 元件，並將其登錄在路由上

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import LoginView from "../views/LoginView.vue";
+import RegisterView from "@/views/RegisterView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,14 +11,11 @@ const router = createRouter({
       name: "login",
       component: LoginView,
     },
-    // {
-    //   path: "/about",
-    //   name: "about",
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import("../views/AboutView.vue"),
-    // },
+    {
+      path: "/register",
+      name: "register",
+      component: RegisterView,
+    },
   ],
 });
 
