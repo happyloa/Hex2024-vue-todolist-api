a2fb4f5aca8c2000fd69fda56c9fec87e5a3dffa
AaronHsieh <piafyoyo06@gmail.com>
2024-08-22 11:09:01 +0800

    新增切換 tab 來顯示對應狀態的代辦事項的功能

--- src/components/todos/TodoListItem.vue
+++ src/components/todos/TodoListItem.vue
@@ -1,11 +1,12 @@
 <script setup>
-import { ref, onMounted } from "vue";
+import { ref, onMounted, computed } from "vue";
 import axios from "axios";
 
 // 定義待辦事項相關的 refs
 const todos = ref([]);
 const todoEdit = ref({});
 const editingId = ref(null); // 追踪正在編輯的待辦事項 ID
+const activeTab = ref("all"); // 定義當前選取的 tab
 
 // 讀取 Cookie 中的 Token
 const getCookie = (name) => {
@@ -85,18 +86,16 @@ const toggleStatus = async (id) => {
   getTodos();
 };
 
-// 設置為編輯模式
-const enableEdit = (id, content) => {
-  editingId.value = id;
-  todoEdit.value[id] = content;
-};
-
-// 處理鍵盤事件
-const handleKeydown = (event, id) => {
-  if (event.key === "Enter") {
-    updateTodo(id);
+// 選取 tab 來過濾待辦事項
+const filteredTodos = computed(() => {
+  if (activeTab.value === "all") {
+    return todos.value;
+  } else if (activeTab.value === "pending") {
+    return todos.value.filter((todo) => !todo.status);
+  } else if (activeTab.value === "completed") {
+    return todos.value.filter((todo) => todo.status);
   }
-};
+});
 
 // 元件掛載後取得待辦事項列表
 onMounted(() => {
@@ -107,14 +106,35 @@ onMounted(() => {
 <template>
   <div class="todoList_list">
     <ul class="todoList_tab">
-      <li><a href="#" class="active">全部</a></li>
-      <li><a href="#">待完成</a></li>
-      <li><a href="#">已完成</a></li>
+      <li>
+        <a
+          href="#"
+          :class="{ active: activeTab === 'all' }"
+          @click.prevent="activeTab = 'all'"
+          >全部</a
+        >
+      </li>
+      <li>
+        <a
+          href="#"
+          :class="{ active: activeTab === 'pending' }"
+          @click.prevent="activeTab = 'pending'"
+          >待完成</a
+        >
+      </li>
+      <li>
+        <a
+          href="#"
+          :class="{ active: activeTab === 'completed' }"
+          @click.prevent="activeTab = 'completed'"
+          >已完成</a
+        >
+      </li>
     </ul>
     <div class="todoList_items">
       <ul class="todoList_item">
-        <!-- 使用 v-for 迭代待辦事項列表，顯示每個待辦事項 -->
-        <li v-for="todo in todos" :key="todo.id">
+        <!-- 使用 v-for 迭代並顯示過濾後的待辦事項 -->
+        <li v-for="todo in filteredTodos" :key="todo.id">
           <label class="todoList_label">
             <input
               class="todoList_input"
@@ -139,7 +159,9 @@ onMounted(() => {
         </li>
       </ul>
       <div class="todoList_statistics">
-        <p>{{ todos.filter((todo) => todo.status).length }} 個已完成項目</p>
+        <p>
+          {{ filteredTodos.filter((todo) => todo.status).length }} 個已完成項目
+        </p>
       </div>
     </div>
   </div>
