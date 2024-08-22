7e41b0a99a804b076912920c8af858655d92c408
AaronHsieh <piafyoyo06@gmail.com>
2024-08-22 10:36:41 +0800

    新增代辦列表元素

---
+++ src/components/todos/TodoListItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="todoList_list">
+    <ul class="todoList_tab">
+      <li><a href="#" class="active">全部</a></li>
+      <li><a href="#">待完成</a></li>
+      <li><a href="#">已完成</a></li>
+    </ul>
+    <div class="todoList_items">
+      <ul class="todoList_item">
+        <li>
+          <label class="todoList_label">
+            <input class="todoList_input" type="checkbox" value="true" />
+            <span>把冰箱發霉的檸檬拿去丟</span>
+          </label>
+          <img src="/src/assets/icons/delete.svg" />
+        </li>
+      </ul>
+      <div class="todoList_statistics">
+        <p>1 個已完成項目</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+@import url("/src/assets/css/TodoListItemStyle.css");
+</style>
