684af44347b7a924be06f84afad2255fba5990f3
AaronHsieh <piafyoyo06@gmail.com>
2024-08-22 11:33:10 +0800

    引入並使用無項目裝飾元件

--- src/components/todos/TodoListContent.vue
+++ src/components/todos/TodoListContent.vue
@@ -1,12 +1,14 @@
 <script setup>
 import TodoInput from "./TodoInput.vue";
 import TodoListItem from "./TodoListItem.vue";
+import TodoNoItem from "./TodoNoItem.vue";
 </script>
 
 <template>
   <main class="todoList_Content">
     <TodoInput />
     <TodoListItem />
+    <TodoNoItem />
   </main>
 </template>
 
